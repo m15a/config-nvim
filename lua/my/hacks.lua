8d4a85e1db042b64a3f9e1e6427588022e71ec90
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-05-30 16:07:02 +0900

    Import more hacks from old init.vim

--- lua/my/hacks.lua
+++ lua/my/hacks.lua
@@ -9,3 +9,26 @@ o.shell = env.SHELL
 u.augroup('remember_last_cursor_position', function(au)
   au [[BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exec "normal g`\"" | endif]]
 end)
+
+-- Practical Vim, Tip 42: '%%' expands to '%:h'
+vim.api.nvim_set_keymap('c', '%%', [[getcmdtype() == ':' ? expand('%:h') . '/' : '%%']],
+                        {expr = true, noremap = true})
+
+-- Practical Vim, Tip 93: Repeat the last substitution by '&'
+for _, mode in ipairs({'n', 'x'}) do
+  vim.api.nvim_set_keymap(mode, '&', [[:&&<CR>]], {noremap = true})
+end
+
+if vim.fn.executable('rg') == 1 then
+  o.grepprg = table.concat({
+    'rg',
+    '--smart-case',
+    '--with-filename',
+    '--no-heading',
+    '--vimgrep',
+  }, ' ')
+  o.grepformat = {
+    '%f:%l:%c:%m',
+    '%f',
+  }
+end
