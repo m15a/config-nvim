1d4b599d63695af4e02fa13991e636a1388325f7
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-05-30 16:41:52 +0900

    Set shiftwidth=2 for filetype vim

--- lua/my/filetypes.lua
+++ lua/my/filetypes.lua
@@ -4,6 +4,10 @@ u.augroup('filetype_lua', function(au)
   au [[FileType lua setlocal shiftwidth=2]]
 end)
 
+u.augroup('filetype_vim', function(au)
+  au [[FileType vim setlocal shiftwidth=2]]
+end)
+
 u.augroup('filetype_make', function(au)
   au [[FileType make setlocal tabstop=4]]
 end)
