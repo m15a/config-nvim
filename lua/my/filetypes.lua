c1f1f9579b67f7a013d96560ee2b64f3eaecce9c
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-05-30 16:26:52 +0900

    Set softtabstop=4 for filetype make

--- lua/my/filetypes.lua
+++ lua/my/filetypes.lua
@@ -3,3 +3,7 @@ local u = require'my.utils'
 u.augroup('filetype_lua', function(au)
   au [[FileType lua setlocal shiftwidth=2]]
 end)
+
+u.augroup('filetype_make', function(au)
+  au [[FileType make setlocal tabstop=4]]
+end)
