26a35c07cd9f476c3eb1aa3a6adde8034d26d9e0
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-11-17 16:04:17 +0900

    Add sections in the plugin list

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -1,26 +1,70 @@
 vim.cmd 'packadd paq-nvim'
 require'paq-nvim' {
+  --[[
+  Sections below are mostly based on
+  [rockerBOO/awesome-neovim](https://github.com/rockerBOO/awesome-neovim).
+  ]]
+  -- Plugin manager {{{1
   {'savq/paq-nvim', opt = true},
-  'chaoren/vim-wordmotion',
-  'dkarter/bullets.vim',
-  'famiu/feline.nvim',
-  'houtsnip/vim-emacscommandline',
-  'kyazdani42/nvim-web-devicons',
-  'lewis6991/gitsigns.nvim',
-  'machakann/vim-sandwich',
+
+  -- LSP {{{1
   'neovim/nvim-lspconfig',
-  'nvim-lua/plenary.nvim',
+
+  -- Markdown {{{1
+  'npxbr/glow.nvim',
+
+  -- Syntax {{{1
   'nvim-treesitter/nvim-treesitter',
-  'rhysd/clever-f.vim',
+  'machakann/vim-sandwich',
+
+  -- Colorscheme {{{1
   'srcery-colors/srcery-vim',
+
+  -- Icon {{{1
+  'kyazdani42/nvim-web-devicons',
+
+  -- Neovim Lua development {{{1
+  'nvim-lua/plenary.nvim',
+
+  -- Statusline {{{1
+  'famiu/feline.nvim',
+
+  -- Git {{{1
+  'lewis6991/gitsigns.nvim',
+
+  -- Comment {{{1
+  'tyru/caw.vim',
+
+  -- Motion {{{1
+  'chaoren/vim-wordmotion',
+  'rhysd/clever-f.vim',
+
+  -- Search {{{1
   'thinca/vim-visualstar',
+
+  -- Editing support {{{1
+  'dkarter/bullets.vim',
   'tpope/vim-repeat',
   'tpope/vim-unimpaired',
-  'tyru/caw.vim',
+
+  -- Command line {{{1
+  'houtsnip/vim-emacscommandline',
+
+  -- Language specific {{{1
+  -- Nix {{{2
   'LnL7/vim-nix',
-  'NovaDev94/vim-fish',
-  'cespare/vim-toml',
+
+  -- Bash {{{2
   'aliou/bats.vim',
+
+  -- Fish {{{2
+  'NovaDev94/vim-fish',
+
+  -- Fennel {{{2
   'mnacamura/vim-fennel-syntax',
-  'npxbr/glow.nvim',
+
+  -- TOML {{{2
+  'cespare/vim-toml',
+  -- }}}1
 }
+-- vim: fdm=marker
