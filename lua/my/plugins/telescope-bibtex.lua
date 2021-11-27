c9e941d3ed397af0180ad52d80d89a39297f0029
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-11-27 15:36:14 +0900

    telescope: configure key maps

--- lua/my/plugins/telescope-bibtex.lua
+++ lua/my/plugins/telescope-bibtex.lua
@@ -1,1 +1,5 @@
+local map = require'my.utils.telescope'.set_keymap
+
 require'telescope'.load_extension('bibtex')
+
+-- map('b', '<Cmd>Telescope bibtex<CR>')
--- lua/my/plugins/telescope.lua
+++ lua/my/plugins/telescope.lua
@@ -1,4 +1,9 @@
-local map = vim.api.nvim_set_keymap
+local map = require'my.utils.telescope'.set_keymap
 
-map('', '[telescope]', '<Nop>', {noremap = true})
-map('', '<Leader>e', '[telescope]', {})
+vim.api.nvim_set_keymap('', '[telescope]', '<Nop>', {noremap = true})
+vim.api.nvim_set_keymap('', '<Leader>e', '[telescope]', {})
+vim.api.nvim_set_keymap('n', '<Leader>E', '<Cmd>Telescope builtin<CR>', {silent = true})
+
+map('b', '<Cmd>Telescope buffers<CR>')
+map('f', '<Cmd>Telescope find_files<CR>')
+map('e', '<Cmd>Telescope resume<CR>')
