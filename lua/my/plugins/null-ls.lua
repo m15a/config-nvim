32b02a0f84f437b94c815a049333fad5b09fc512
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-02 12:50:30 +0900

    null-ls: add more sources

--- lua/my/plugins/null-ls.lua
+++ lua/my/plugins/null-ls.lua
@@ -23,12 +23,36 @@ local function has_any_file(names)
   end
 end
 
+local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 null_ls.config({
   sources = {
+    -- General
+    code_actions.gitsigns,
+
+    -- Vim
+    diagnostics.vint.with({ condition = has_any_file({ ".vintrc.yml", ".vintrc" }) }),
+
+    -- Lua
     diagnostics.selene.with({ condition = has_file("selene.toml") }),
     formatting.stylua.with({ condition = has_any_file({ "stylua.toml", ".stylua.toml" }) }),
+
+    -- Nix
+    -- code_actions.statix.with({ condition = has_file("statix.toml") }),
+    -- formatting.nixfmt.with({ condition = has_executable("nixfmt") }),
+
+    -- Bash
+    diagnostics.shellcheck.with({ condition = has_file(".shellcheckrc") }),
+    -- formatting.shfmt.with({ condition = has_file(".editorconfig") }),
+
+    -- Fish
+    formatting.fish_indent.with({ condition = has_executable("fish") }),
+
+    -- Python
+    diagnostics.flake8.with({ condition = has_file("pyproject.toml") }),
+    formatting.black.with({ condition = has_file("pyproject.toml") }),
+    formatting.isort.with({ condition = has_file("pyproject.toml") }),
   },
 })
