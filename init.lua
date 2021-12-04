a932c63e6a8db8d61e096590c0346d8b98c0ca38
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-04 18:39:53 +0900

    Keymaps esp. map(local)leader should be set before plugin config

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local require_ = require('my.utils').require
 
 require_ 'my.options'
-require_ 'my.plugins'
 require_ 'my.keymaps'
+require_ 'my.plugins'
 require_ 'my.filetypes'
 require_ 'my.hacks'
