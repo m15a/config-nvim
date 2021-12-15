7e168f1f1b7f99b330463152a653abdd6da70b20
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-15 21:35:45 +0900

    chore: embed shell.nix into flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,26 @@
 {
-  description = "Development tools for mnacamura/config-nvim";
+  description = "An example of neovim configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, neovim-nightly-overlay, ... }: {
+    overlay = import ./overlay.nix;
+  } // (flake-utils.lib.eachDefaultSystem (system:
   let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    devShell = import ./shell.nix { inherit pkgs; };
-  });
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ self.overlay ];
+    };
+  in {
+    devShell = pkgs.mkShell {
+      buildInputs = [
+        pkgs.selene
+        pkgs.stylua
+      ];
+    };
+  }));
 }
