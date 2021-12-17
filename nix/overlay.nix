822006319fdf4d87bbcbf0c109f6b18a02b38dc1
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-17 15:19:51 +0900

    fix: patch feline

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -12,4 +12,16 @@ in
     neovim = final.neovim-nightly;
     inherit config-nvim;
   };
+
+  vimExtraPlugins = prev.vimExtraPlugins.extend (self: super: {
+    feline-nvim = super.feline-nvim.overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        # https://github.com/famiu/feline.nvim/pull/177
+        (final.fetchpatch {
+          url = "https://github.com/mnacamura/feline.nvim/commit/2b2825fc46ff841d377720e4aa5326a290b34701.patch";
+          sha256 = "14h9jp14hk93547b13i543h3k22qdwblkas83812ylvzpihx8lrc";
+        })
+      ];
+    });
+  });
 }
