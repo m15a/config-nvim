5e3c8bb873068c6e651c2f94ac158385527622c6
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-12-21 13:56:53 +0900

    fix: update nixpkgs-vim-plugins

--- nix/pkgs/my-neovim.nix
+++ nix/pkgs/my-neovim.nix
@@ -47,8 +47,8 @@ wrapNeovim neovim {
         vimExtraPlugins.bats-vim
         vimExtraPlugins.bullets-vim
         vimExtraPlugins.dial-nvim
-        vimExtraPlugins.feline-nvim
-        vimExtraPlugins.iron-nvim
+        vimExtraPlugins.feline-nvim-develop
+        vimExtraPlugins.iron-nvim-mnacamura
         vimExtraPlugins.lspactions
         vimExtraPlugins.nvim-lastplace
         vimExtraPlugins.nvim-pqf
