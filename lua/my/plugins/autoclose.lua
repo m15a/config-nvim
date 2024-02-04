95fac0fbb5bcaae4150713fac4c37d7aa9297173
NAKAMURA Mitsuhiro <m.nacamura@gmail.com>
2024-02-04 12:21:02 +0900

    fix(autoclose): tweak config

--- lua/my/plugins/autoclose.lua
+++ lua/my/plugins/autoclose.lua
@@ -1,9 +1,31 @@
 require('autoclose').setup {
-   -- Leave Lispy languages to parinfer.
-   disabled_filetypes = {
-      'lisp',
-      'scheme',
-      'clojure',
-      'fennel',
+   keys = {
+      ['('] = { escape = false, close = true, pair = '()' },
+      ['['] = { escape = false, close = true, pair = '[]' },
+      ['{'] = { escape = false, close = true, pair = '{}' },
+
+      [')'] = { escape = true, close = false, pair = '()' },
+      [']'] = { escape = true, close = false, pair = '[]' },
+      ['}'] = { escape = true, close = false, pair = '{}' },
+
+      ['"'] = { escape = true, close = true, pair = '""' },
+      -- Single single-quote is used everywhere for variable names (e.g. x'),
+      -- abbreviations (e.g., I don't think so), quasi-quote (e.g. '(a b c)), etc.
+      -- ["'"] = { escape = true, close = true, pair = "''" },
+      ['`'] = { escape = true, close = true, pair = '``' },
+   },
+
+   options = {
+      -- Leave Lispy languages to parinfer.
+      disabled_filetypes = {
+         'lisp',
+         'scheme',
+         'clojure',
+         'fennel',
+      },
+      disable_command_mode = true,
+      disable_when_touch = true,
+      pair_spaces = true,
+      -- I don't think so.
    },
 }
