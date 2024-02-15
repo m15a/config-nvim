22d7ecc7516c4d17aef46cc7ee71d355e86c53b4
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-02-15 14:47:35 +0900

    feat(lspconfig): use fennel language server

---
+++ lua/my/plugins/lspconfig/fennel_ls.lua
@@ -0,0 +1,8 @@
+return {
+   settings = {
+      ['fennel-ls'] = {
+         ['fennel-path'] = './?.fnl;./?/init.fnl;src/?.fnl;src/?/init.fnl',
+         ['macro-path'] = './?.fnl;./?/init-macros.fnl;./?/init.fnl;src/?.fnl;src/?/init-macros.fnl;src/?/init.fnl',
+      },
+   },
+}
